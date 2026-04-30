69c84a2dd1ecd938c8a51182ef4f465bd61b8e53
oferker <oferk@getsat.com>
2026-04-30 16:52:27 +0300

    Update package with mode constants and bulk erase

--- mt25q_qspi_pkg.vhd
+++ mt25q_qspi_pkg.vhd
@@ -19,8 +19,20 @@ package mt25q_qspi_pkg is
     constant CMD_QUAD_IO_FAST_READ     : std_logic_vector(7 downto 0) := x"EB";  -- 4-4-4
     constant CMD_PAGE_PROGRAM          : std_logic_vector(7 downto 0) := x"02";  -- 4-4-4 in quad mode
 
+    -- Bulk Erase
+    constant CMD_BULK_ERASE            : std_logic_vector(7 downto 0) := x"C7";
+
     -- Dummy cycle counts
     constant DUMMY_CYCLES              : integer := 8;   -- for 0x6B (extended SPI)
-    constant DUMMY_CYCLES_QIO          : integer := 10;  -- for 0xEB (2 mode + 8 dummy)
+    constant DUMMY_CYCLES_QIO          : integer := 10;  -- for 0xEB (quad I/O)
+
+    -- Protocol mode constants
+    constant MODE_EXTENDED : std_logic_vector(1 downto 0) := "00";
+    constant MODE_DUAL     : std_logic_vector(1 downto 0) := "01";
+    constant MODE_QUAD     : std_logic_vector(1 downto 0) := "10";
+
+    -- Enhanced Volatile Configuration Register values
+    constant EVCR_DEFAULT  : std_logic_vector(7 downto 0) := x"FF";  -- power-up default
+    constant EVCR_QUAD     : std_logic_vector(7 downto 0) := x"7F";  -- bit7=0 enables quad
 
 end package mt25q_qspi_pkg;
